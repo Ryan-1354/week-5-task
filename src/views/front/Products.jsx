c79f44d0ade77a55c0d148fe25a388c87e7c9fe0
002 <tourrec002@gmail.com>
2026-01-30 14:37:41 +0800

    products page done

--- src/views/front/Products.jsx
+++ src/views/front/Products.jsx
@@ -9,9 +9,10 @@ export default function Products() {
     const getProduct = async () => {
       try {
         const res = await axios.get(`${API_BASE}/v2/api/${API_PATH}/products`);
-        console.log(res);
+        console.log(res.data.products);
+        setProducts(res.data.products);
       } catch (error) {
-        console.log(error.response);
+        console.log(error.data.products);
       }
     };
     getProduct();
@@ -19,24 +20,26 @@ export default function Products() {
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-4 mb-3">
-          <div className="card">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-              <h5 className="card-title">Card title</h5>
-              <p className="card-text">
-                This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
-                longer.
-              </p>
-              <p className="card-text">
-                <small className="text-body-secondary">
-                  Last updated 3 mins ago
-                </small>
-              </p>
+        {products.map((product) => (
+          <div className="col-md-4 mb-3" key={product.id}>
+            <div className="card">
+              <img
+                src={product.imageUrl}
+                className="card-img-top"
+                style={{ maxHeight: "200px", objectFit: "cover" }}
+                alt={product.description}
+              />
+              <div className="card-body">
+                <h5 className="card-title">{product.title}</h5>
+                <p className="card-text">{product.content}</p>
+                <p className="card-text">{product.description}</p>
+                <p className="card-text">價格：{product.price}</p>
+                <p className="card-text">{product.unit}</p>
+                <button className="btn btn-primary">View</button>
+              </div>
             </div>
           </div>
-        </div>
+        ))}
       </div>
     </div>
   );
