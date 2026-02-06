76cad7284aa3df0039916bb6e08b5f4b86d686e1
002 <tourrec002@gmail.com>
2026-02-06 15:35:41 +0800

    enviroment setUp

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/week-5-task/",
+  base: "/week-6-task/",
   plugins: [react()],
 });
